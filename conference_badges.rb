b58c82558e17e984aa9630224d50f05259de0068
brian ambundo <brian.katiechi9@gmail.com>
2022-08-18 18:00:11 +0300

    commit tree 971e1e8cd403d9a09d95ed6a40446e3b277d38a3

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker (name)
+    "Hello, my name is #{name}."
+end
+puts badge_maker("Ambundo") 
+
+def batch_badge_creator(array)
+    array.map do |name|
+        "Hello, my name is #{name}." 
+    end 
+end
+puts batch_badge_creator(["Arel", "Carol"])
+
+def assign_rooms(array)
+    array.each_with_index.map do |name, idx|
+        "Hello, #{name}! You'll be assigned to room #{idx+1}!"
+    
+    end
+end
+puts assign_rooms(["Arel", "Carol"])
+
+def printer (array)
+    batch_badge_creator(array).each do |i|
+       puts i
+    end
+    assign_rooms(array).each do |j|
+       puts j
+    end
+end
+puts printer(["Arel", "Carol"])
